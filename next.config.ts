a458d10d1492974c9d85d75d4e693f45b778f9ba
Saimum Admin <admin@saimum.org>
2026-09-20 16:03:57 +0600

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Copyright-Act-2023-Bangladesh",
+  images: {
+    unoptimized: true, // Required for static export
+  }
 };
 
 export default nextConfig;
